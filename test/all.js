b6c38c0ce851e4860f45ed8d8a44a2c1645de80c
matthewhudson <matthud@gmail.com>
2019-11-01 16:04:37 -0400

    fix spawn sync call.

--- test/all.js
+++ test/all.js
@@ -4,7 +4,7 @@
 const assert = require('assert').strict
 const { readdirSync } = require('fs')
 const { join } = require('path')
-const spawnSync = require('cross-spawn')
+const spawnSync = require('cross-spawn').sync
 
 const payloadDir = join(__dirname, 'payloads')
 const runner = join(__dirname, 'runner.js')
